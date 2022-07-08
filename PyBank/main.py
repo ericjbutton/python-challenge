96a4effd3f899dd385fd4a07a600c8b3b3c91b23
Eric Button <eric.button@protonmail.com>
2022-07-08 10:15:25 -0500

    Finished Homework 3, added notes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,35 +1,55 @@
+# Import OS module and module for reading CSV
+
 import os
 import csv
 
 budget_data = os.path.join('Resources','budget_data.csv')
 
+# Set variables
+
 total_months = []
 total = 0
 profit_loss_list = []
-months_year = []
 monthly_change_list = []
 total_change = 0
 num1 = 0
 num2 = 1
 
+# Header for summary tables
+
 print('Financial Analysis')
 
 print('--------------------')
+
+# Improve reading using CSV module
 
 with open(budget_data) as csvfile:
 
     csv_reader = csv.reader(csvfile, delimiter=',')
 
+    # Skip Headers
+
     next(csv_reader,None)
+
+    # Read each row of CSV file
 
     for row in csv_reader:
 
-        total_months.append(row[0])
-        total = total + int(row[1])
-        profit_loss_list.append(int(row[1]))
-        months_year.append(row[0])
+    # For each row in column 0, append total months list
 
-    monthly_change_list = [0]
+        total_months.append(row[0])
+
+    # For each row in column 1, add total profit/loss
+
+        total = total + int(row[1])
+
+    # For each row in column 1, append profit/loss list
+
+        profit_loss_list.append(int(row[1]))
+
+    # While number 2 is less than 86, subtract first number in profit/loss list from second number in profit/loss list
+    # Add monthly change to monthly change list and add to total change list
+    # Add 1 to each number for each loop
 
     while len(total_months) > num2:
         monthly_change = profit_loss_list[num2] - profit_loss_list[num1]
@@ -37,14 +57,18 @@ with open(budget_data) as csvfile:
         total_change = total_change + monthly_change
         num1 = num1 + 1
         num2 = num2 + 1
-        
+    
+#Calculate average change by taking total change over total months - 1 (because there is 1 less loop than total)
+
     average_change = round(total_change / (len(total_months)-1),2)
+
+#Print summary table
 
     print(f'Total Months: {len(total_months)}')
     print(f'Total: ${total}')
-    print(f'Average Change: {average_change}')
-    print(f'Greatest Increase in Profit: {months_year[monthly_change_list.index(max(monthly_change_list))]} (${max(monthly_change_list)})')
-    print(f'Greatest Decrease in Profit: {months_year[monthly_change_list.index(min(monthly_change_list))]} (${min(monthly_change_list)})')
+    print(f'Average Change: ${average_change}')
+    print(f'Greatest Increase in Profits: {total_months[monthly_change_list.index(max(monthly_change_list))]} (${max(monthly_change_list)})')
+    print(f'Greatest Decrease in Profits: {total_months[monthly_change_list.index(min(monthly_change_list))]} (${min(monthly_change_list)})')
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,40 +1,67 @@
+
+#Import OS module and module for reading CSV
 import os
 import csv
 
 election_data = os.path.join('Resources','election_data.csv')
+
+# Header for summary table
+
 print()
 print('Election Results')
 print('--------------------')
+
+# Set Variables for votes
 
 total_votes = 0
 Charles_Casper_Stockham_votes = 0
 Diana_DeGette_votes = 0
 Raymon_Anthony_Doane_votes = 0
 
+# Improve reading using CSV module
 
 with open(election_data) as csvfile:
 
     csv_reader = csv.reader(csvfile, delimiter=',')
 
+# Skip Headers
+
     next(csv_reader,None)
 
+# Read each row of CSV file
+
     for row in csv_reader:
+
+# Count total votes
+
         total_votes = total_votes + 1
-        
+
+    # If row of 2nd column of CSV file = Charles Casper Stockham, add 1 vote for Carles Casper Stockham   
+
         if row[2] == 'Charles Casper Stockham':
             Charles_Casper_Stockham_votes = Charles_Casper_Stockham_votes + 1
+
+    # If row of 2nd column of CSV file = Diana DeGette, add 1 vote for Diana DeGette
 
         elif row[2] == 'Diana DeGette':
             Diana_DeGette_votes = Diana_DeGette_votes + 1
 
+    # If else, add 1 vote for Raymon Anthony Doane
+
         else:
             Raymon_Anthony_Doane_votes = Raymon_Anthony_Doane_votes + 1
+
+    # Calculate and format percentage of total vote for each candidate
 
     Charles_Casper_Stockham_percent = round((Charles_Casper_Stockham_votes / total_votes) * 100,3)
     Diana_DeGette_percent = round((Diana_DeGette_votes / total_votes) * 100,3)
     Raymon_Anthony_Doane_percent = round((Raymon_Anthony_Doane_votes / total_votes) * 100,3)
-    
+
+    # Find max votes
+
     most_votes = max(Charles_Casper_Stockham_votes,Diana_DeGette_votes,Raymon_Anthony_Doane_votes)
+
+    # Determine Winner
 
     if most_votes == Charles_Casper_Stockham_votes:
         winner = 'Charles Casper Stockham'
@@ -42,7 +69,9 @@ with open(election_data) as csvfile:
         winner = 'Diana DeGette'
     else:
         winner = 'Raymon Anthony Doane'
-    
+
+    # Print summary 
+
     print(f'Total Votes: {total_votes}')
     print('--------------------')
     print(f'Charles Casper Stockham: {Charles_Casper_Stockham_percent}% ({Charles_Casper_Stockham_votes})')
